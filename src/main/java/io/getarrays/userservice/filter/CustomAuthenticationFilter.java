435cab253c2732c918b7357cd6099f0665817d50
Yonghoon Lee <dldydgns53@naver.com>
2022-07-07 23:55:10 +0900

    feat: access_token, refresh_token 추가
    - auth0:java-jwt 의존성 추가
    - security configure 설정 추가

--- src/main/java/io/getarrays/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/io/getarrays/userservice/filter/CustomAuthenticationFilter.java
@@ -1,6 +1,15 @@
 package io.getarrays.userservice.filter;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +19,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
@@ -33,10 +44,38 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         return authenticationManager.authenticate(authenticationToken);
     }
-    ㄴ
+
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
         // 로그인 성공시 사용자에게 토큰 또는 액세스 토큰과 refresh 토큰을 제공해야 한다.
-        super.successfulAuthentication(request, response, chain, authResult);
+        User user = (User) authentication.getPrincipal(); // 성공적으로 인증된 사용자 객체를 반환
+        // 이제 사용자에 대한 접근이 가능하므로 사용자로 부터 정보를 가져와 JWT 을 생성할 수 있다.
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String access_token = JWT.create()
+            .withSubject(user.getUsername())
+            .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) // 10분으로 설정
+            .withIssuer(request.getRequestURI().toString())
+            .withClaim("roles", user.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()))
+            .sign(algorithm);
+
+        String refresh_token = JWT.create()
+            .withSubject(user.getUsername())
+            .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)) // 30분
+            .withIssuer(request.getRequestURI().toString())
+            .sign(algorithm);
+
+        // 헤더에 적용
+//        response.setHeader("access_token", access_token);
+//        response.setHeader("refresh_token", refresh_token);
+
+        // json 으로 body 적용
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
--- src/main/java/io/getarrays/userservice/security/SecurityConfig.java
+++ src/main/java/io/getarrays/userservice/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package io.getarrays.userservice.security;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
 import io.getarrays.userservice.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,10 +33,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        // 여기서 URL 을 변경할 수 있다.(custom 하게 설정)
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll(); // 이순간 누구나 접근 가능
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+
+
+        //(순서1)
+        http.authorizeRequests().antMatchers("api/login/**").permitAll(); // 로그인은 누구나 접근 가능하게
+        //(순서2)
+        http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER"); // ** 는 뒤에 오는 모든것 의미
+        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+//        http.authorizeRequests().anyRequest().permitAll(); // 이순간 누구나 접근 가능 - 보안상 좋지 않다.
+        //(순서3)
+        http.authorizeRequests().anyRequest().authenticated(); // 권한설정(인증) - 이때 권한 설정 순서가 중요하다.
+
+
+
+        http.addFilter(customAuthenticationFilter);
     }
 
     @Bean
